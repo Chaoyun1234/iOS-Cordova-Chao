539993be92b04057e1562a6d4105199caf113e4f
bys <bys@bysdeMBP-2.guest.corp.microsoft.com>
2018-04-17 16:15:16 +0800

    CordovaProject

---
+++ platforms/ios/Pods/Target Support Files/Pods-iOSCodCh/Pods-iOSCodCh-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSCodCh : NSObject
+@end
+@implementation PodsDummy_Pods_iOSCodCh
+@end
---
+++ www/js/index.js
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        //Push notification
+        var onNotificationReceived = function(pushNotification) {
+            var message = pushNotification.message;
+            var title = pushNotification.title;
+            
+            if (message === null || message === undefined) {
+                // Android messages received in the background don't include a message. On Android, that fact can be used to
+                // check if the message was received in the background or foreground. For iOS the message is always present.
+                title = 'Android background';
+                message = '<empty>';
+            }
+            
+            // Custom name/value pairs set in the App Center web portal are in customProperties
+            if (pushNotification.customProperties && Object.keys(pushNotification.customProperties).length > 0) {
+                message += '\nCustom properties:\n' + JSON.stringify(pushNotification.customProperties);
+            }
+            
+            console.log(title, message);
+        }
+        
+        AppCenter.Push.addEventListener('notificationReceived', onNotificationReceived);
+        this.receivedEvent('deviceready');
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
